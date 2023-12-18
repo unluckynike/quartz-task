1dc1063ce5af7a6b1d4307c93bf86cb4fb0b0909
WuLiZeng <2230432084@qq.com>
2023-12-18 10:17:39 +0800

    remove target

--- target/classes/sqlfile/test.sql
+++
@@ -1,6 +0,0 @@
-select * from user
-SELECT SUM(column_name) AS total_sum FROM user
-SELECT AVG(column_name) AS average_value FROM user
-SELECT t1.*, t2.* FROM table1 t1 INNER JOIN table2 t2 ON t1.id = t2.table1_id;
-DELETE FROM your_table_name WHERE condition;
-SELECT * FROM user ORDER BY column_name ASC;
